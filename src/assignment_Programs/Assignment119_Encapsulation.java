404b4d161505f73aac1e938eaecc7c885d9e1d98
devidasyadav <devidasyadav151094@gmail.com>
2025-03-13 19:56:56 +0530

    Commiting Assignment119_body

--- src/assignment_Programs/Assignment119_Encapsulation.java
+++ src/assignment_Programs/Assignment119_Encapsulation.java
@@ -2,9 +2,18 @@ package assignment_Programs;
 
 public class Assignment119_Encapsulation 
 {//WAP on Encapsulation
+	
+	private String name;
+
+    // Getter and Setter for name
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
 	public static void main(String[] args) 
 	{
-		
+		Assignment119_Encapsulation p = new Assignment119_Encapsulation();
+        p.setName("Geek"); 
+        System.out.println("Name=> " + p.getName());
 	}
 
 }
